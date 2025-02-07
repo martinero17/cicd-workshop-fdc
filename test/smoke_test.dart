e56654df6eee1eca09d68fbf69ef5f1fc4e1a10c
Martim Francisco <a46912@alunos.isel.pt>
2025-02-07 17:56:24 +0000

    Removed unnecessary extra comment

--- test/smoke_test.dart
+++ test/smoke_test.dart
@@ -39,7 +39,6 @@ void main() {
     await tester.pumpAndSettle();
 
     // Verify we're back on the homepage.
-    // This is used to create a branch.
     expect(find.text('Play'), findsOneWidget);
   });
 }
